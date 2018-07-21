46be521c03defcbbe4717b8766ca818e2d1d7d61
sachinlamba <lamba92sachin@gmail.com>
2018-07-21 14:27:37 +0530

    message box with blur-.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-message-preview
 Browser centered Message box with blur background.
+
+# Help
+How to Publish Guide [here](http://jasonwatmore.com/post/2018/04/14/react-npm-how-to-publish-a-react-component-to-npm).
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
  
 const propTypes = {
-    styles: PropTypes.object
+    details: PropTypes.object
 }
  
 const defaultProps = {
-    styles: {
-        backgroundDetails: {
+    details: {
+        backgroundStyle: {
             position: "fixed",
             display: "block",
             background: "black",
@@ -15,20 +15,59 @@ const defaultProps = {
             top: 0,
             left: 0,
             right: 0,
-            bottom: 0
+            bottom: 0,
+            zIndex: 999
+        },
+        messageStyle: {
+          position: "fixed",
+          zIndex: 1000,
+          background: "white",
+          left: "40%",
+          top: "45%",
+          fontSize: "16px",
+          padding: "12px 30px",
+          border: "1px solid #d4d4d4",
+          borderRadius: "10px"
+        },
+        crossButtonStyle: {
+          float: "right",
+          position: "absolute",
+          top: "-5px",
+          right: "8px",
+          fontSize: "20px"
+        },
+        messageText: "Pass your text with props Name as messageText...",
+        cancelButton: {
+          margin: "auto",
+          padding: "0px 10px",
+          borderRadius: "20px",
+          background: "lightcoral",
+          color: "white"
+        },
+        okButton: {
+          margin: "auto",
+          padding:" 0px 10px",
+          borderRadius: "20px",
+          background: "lightblue",
+          color: "white"
         }
     }
 }
 
 class MessagePreviewer extends React.Component {     
     render() {
-        const styles = this.props.styles || {};
-        return (
-            <div>
-                <div style={styles.backgroundDetails}></div>
-                <label>Welcome to x`x`</label>
-            </div>
-        );
+        const details = this.props.details || {};
+        return [
+                <div style={details.backgroundStyle}></div>,
+                <div style={details.messageStyle}>
+                  <div style={details.crossButtonStyle}>&times;</div>
+                  <div>{details.messageText}</div>
+                  <div style={{display: "flex"}}>
+                    <div style={details.cancelButton}>Cancel</div>
+                    <div style={details.okButton}>Ok</div>
+                  </div>
+                </div>
+            ];
     }
 }
  
